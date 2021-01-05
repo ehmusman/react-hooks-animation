7d713293f8578cfc5b2abd4cc5ca322797ee6db5
ehmusman <ehmusman@gmail.com>
2021-01-06 00:34:23 +0500

    rotation is also added

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ function App() {
 
   const { ref, playState, getAnimation } = useWebAnimations({
     keyframes: [
-      { transform: 'translate(0,0)' },
-      { transform: 'translate(150px,0)' },
-      { transform: 'translate(150px,150px)' },
-      { transform: 'translate(300px,150px)' },
-      { transform: 'translate(300px,300px)' },
-      { transform: 'translate(450px,300px)' },
-      { transform: 'translate(450px,450px)' },
-      { transform: 'translate(600px,450px)' },
-      { transform: 'translate(600px,600px)' },
-      { transform: 'translate(750px,600px)' },
-      { transform: 'translate(750px,650px)' },
-      { transform: 'translate(0px,650px)' },
-      { transform: 'translate(0px,0px)' },
+      { transform: 'translate(0,0) rotate(0)' },
+      { transform: 'translate(150px,0) rotate(360deg)' },
+      { transform: 'translate(150px,150px) rotate(720deg)' },
+      { transform: 'translate(300px,150px) rotate(1080deg)' },
+      { transform: 'translate(300px,300px) rotate(1440deg)' },
+      { transform: 'translate(450px,300px) rotate(1800deg)' },
+      { transform: 'translate(450px,450px) rotate(2160deg)' },
+      { transform: 'translate(600px,450px) rotate(2520deg)' },
+      { transform: 'translate(600px,600px) rotate(2880deg)' },
+      { transform: 'translate(750px,600px) rotate(3240deg)' },
+      { transform: 'translate(750px,650px) rotate(3600deg)' },
+      { transform: 'translate(0px,650px) rotate(3960deg)' },
+      { transform: 'translate(0px,0px) rotate(0deg)' },
     ],
     timing: {
-      duration: 3000,
+      duration: 6000,
       direction: "alternate"
     },
   });
