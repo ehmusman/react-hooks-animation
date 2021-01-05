c6d70b52fde2c7f64243a410d78c5a1734a5a986
ehmusman <ehmusman@gmail.com>
2021-01-06 00:17:07 +0500

    range input is removed

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Getting Started with animation using react hooks
 
 a ball is falling down animation, App is deployed on following link
-http://shoe-store-ehmusman.surge.sh/
+react-hooks-animation-ehmusman.surge.sh
--- src/App.js
+++ src/App.js
@@ -5,10 +5,6 @@ import useWebAnimations from "@wellyshen/use-web-animations";
 function App() {
   const element = useRef(null)
   const [value, setValue] = useState(2.5)
-  useEffect(() => {
-    getAnimation().playbackRate = value
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [value])
   const handleChange = (e) => {
     setValue(e.target.value)
   }
@@ -40,10 +36,6 @@ function App() {
         <button className="play" onClick={() => getAnimation().play()}>Play</button>
         <button className="pause" onClick={() => getAnimation().pause()}>Pause</button>
         <button className="reverse" onClick={() => getAnimation().reverse()}>Reverse</button>
-        <div className='changing'>
-          <input type="range" onChange={handleChange} ref={element} min='0.5' max='5' step='0.5' value={value} className="range" />
-          <h3>{value}</h3>
-        </div>
 
       </div>
       <div className="target" ref={ref} >
